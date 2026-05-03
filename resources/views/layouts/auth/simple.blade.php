6281a19282d0424bc09dc4f9295249efd7793683
IqraJr <iqrafauzanakbar@gmail.com>
2026-05-04 00:14:14 +0800

    agar tampilan ubah password konsisten

--- resources/views/layouts/auth/simple.blade.php
+++ resources/views/layouts/auth/simple.blade.php
@@ -1,6 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
-    <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ $title ?? 'Auth' }}</title>
+
+    {{--  WAJIB --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    {{-- Livewire --}}
+    @livewireStyles
+</head>
+
+<body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
 
     {{ $slot }}
 
@@ -10,6 +23,9 @@
         </flux:toast.group>
     @endpersist
 
+    {{--  WAJIB --}}
+    @livewireScripts
     @fluxScripts
+
 </body>
 </html>
